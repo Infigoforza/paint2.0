ad76c6ffba03dca9bb48f62afe0a461cc8da8805
Lars <lars.timmerman@student.hu.nl>
2015-02-17 18:47:56 +0100

    randomDraw()

--- src/paint/TekenCanvas.java
+++ src/paint/TekenCanvas.java
@@ -2,18 +2,32 @@ package paint;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class TekenCanvas extends Canvas {
+	
+	private GraphicsContext gc = getGraphicsContext2D();
 
 	public TekenCanvas(int breedte, int hoogte) {
 		super(breedte, hoogte);
-		GraphicsContext gc = getGraphicsContext2D();
-		this.setOnMousePressed(e -> lineTekenen());
+		
+		//this.setOnMouseDragged(e -> randomDraw(e));
+		this.setOnMouseDragged(e -> drawRect(e));
 
 	}
-
-	public void lineTekenen() {
+	
+	//gewoon zelf tekenen, geen figuren.
+/*	public void randomDraw(MouseEvent e) {
+		gc.getPixelWriter().setColor((int)e.getX(),(int)e.getY(), Color.BLACK);
+		gc.getPixelWriter().setColor((int)e.getX(),(int)e.getY() + 1, Color.BLACK);
+		gc.getPixelWriter().setColor((int)e.getX(),(int)e.getY() + 2, Color.BLACK);
 		
+	} */
+	
+		//Een vierkant tekenen
+	public void drawRect(MouseEvent e){
+		gc.lineTo(e.getX(), e.getY());
 	}
 
 }
