91d830fa259a9da346cac47690d0b12e8f0628ef
Lars <lars.timmerman@student.hu.nl>
2015-02-17 16:39:55 +0100

    Canvas

--- src/paint/TekenApp.java
+++ src/paint/TekenApp.java
@@ -1,5 +1,28 @@
 package paint;
 
-public class TekenApp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class TekenApp extends Application {
+	private TekenCanvas tc;
+
+	@Override
+	public void start(Stage primaryStage) {
+
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root, 400, 350);
+
+		root.setCenter(tc);
+
+		primaryStage.setTitle("Paint");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
 
 }
--- src/paint/TekenCanvas.java
+++ src/paint/TekenCanvas.java
@@ -1,5 +1,14 @@
 package paint;
 
-public class TekenCanvas {
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
+
+public class TekenCanvas extends Canvas {
+	private Label lb = new Label("kip");
+
+	public TekenCanvas(int breedte, int hoogte) {
+		super(breedte, hoogte);
+	}
+	
 
 }
