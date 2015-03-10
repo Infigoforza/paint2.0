e76d942ee7cbc2a7a7af4309ec9835ef5b142c4c
MartijnDevNull <m.balsmeer@gmail.com>
2015-03-10 14:05:21 +0100

    Ruimte voor Rectangles

--- src/paint/BedienPaneel.java
+++ src/paint/BedienPaneel.java
@@ -2,8 +2,10 @@ package paint;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -13,20 +15,39 @@ public class BedienPaneel extends HBox {
 	public BedienPaneel() {
 		HBox h1 = new HBox();
 		HBox h2 = new HBox();
-		h1.setPadding(new Insets(15,15,15,12));
+		VBox v1 = new VBox();
+		VBox v2 = new VBox();
+		
+		Rectangle[] recArr = new Rectangle[9];
+//		for (Rectangle rec : recArr){
+//			rec.setWidth(100);
+//			rec.setHeight(30);
+//			rec.setFill(Color.AQUAMARINE);
+//		}
+		
+		Button testButton = new Button("Klik me");
+		
+		h1.setPadding(new Insets(15, 15, 15, 12));
 		cp = new ColorPicker();
 		Rectangle r = new Rectangle();
 		r.setWidth(100);
 		r.setHeight(30);
+		
 		h1.setPrefSize(50, 100);
+		h2.setPrefSize(400, 100);
 		h1.setStyle("-fx-border-color: black;");
-		h1.getChildren().addAll(cp, r);
+		h2.setStyle("-fx-border-color: black;");
+		v1.getChildren().addAll(cp, r);
+		v2.getChildren().addAll(testButton);
+		h1.getChildren().addAll(v1);
 		h1.setAlignment(Pos.CENTER_LEFT);
-		getChildren().addAll(h1);
+		h2.getChildren().add(v2);
+		getChildren().addAll(h1, h2);
+		
 		cp.setOnAction(e -> r.setFill(cp.getValue()));
 	}
-	
-	public static Color getColor(){
+
+	public static Color getColor() {
 		return cp.getValue();
 	}
 }
