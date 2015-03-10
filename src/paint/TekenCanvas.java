bc8fa119c6e33bbe35979b9670298a76f121bf4c
MartijnDevNull <m.balsmeer@gmail.com>
2015-03-10 14:20:45 +0100

    Better draw function

--- src/paint/TekenCanvas.java
+++ src/paint/TekenCanvas.java
@@ -3,31 +3,39 @@ package paint;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 
 public class TekenCanvas extends Canvas {
-	
 	private GraphicsContext gc = getGraphicsContext2D();
 
 	public TekenCanvas(int breedte, int hoogte) {
 		super(breedte, hoogte);
-		
+		this.setOnMouseClicked(e -> randomDraw(e));
 		this.setOnMouseDragged(e -> randomDraw(e));
-		//this.setOnMouseDragged(e -> drawRect(e));
-
+		this.setOnMouseReleased(e -> randomDraw(e));
 	}
-	
-	//gewoon zelf tekenen, geen figuren.
+
+	// gewoon zelf tekenen, geen figuren.
 	public void randomDraw(MouseEvent e) {
-		gc.getPixelWriter().setColor((int)e.getX(),(int)e.getY(), BedienPaneel.getColor());
-		gc.getPixelWriter().setColor((int)e.getX(),(int)e.getY() + 1, BedienPaneel.getColor());
-		gc.getPixelWriter().setColor((int)e.getX(),(int)e.getY() + 2, BedienPaneel.getColor());
+		if(e.getEventType().equals(MouseEvent.MOUSE_CLICKED) ){
+			gc.setStroke(BedienPaneel.getColor()); 
+			gc.beginPath();
+					
+			
+		}
 		
-	} 
-	
-		//Een vierkant tekenen
-	public void drawRect(MouseEvent e){
+		if(e.getEventType().equals(MouseEvent.MOUSE_DRAGGED)){
+			gc.setStroke(BedienPaneel.getColor());
+			gc.lineTo(e.getX(), e.getY());
+			gc.stroke();
+		}
+		
+		if(e.getEventType().equals(MouseEvent.MOUSE_RELEASED)){
+			gc.closePath();
+		}
+	}
+
+	// Een vierkant tekenen
+	public void drawRect(MouseEvent e) {
 		gc.lineTo(e.getX(), e.getY());
 	}
-
 }
