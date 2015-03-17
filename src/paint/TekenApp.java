d54669a3954333553a9ac7a056e40a0876ba6721
Lars <Lars@145.89.165.1>
2015-03-17 12:23:56 +0100

    Menubar

--- src/paint/TekenApp.java
+++ src/paint/TekenApp.java
@@ -2,7 +2,11 @@ package paint;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TekenApp extends Application {
@@ -13,12 +17,32 @@ public class TekenApp extends Application {
 		BedienPaneel bp = new BedienPaneel();
 		BorderPane root = new BorderPane();
 		Scene scene = new Scene(root, 440, 350);
-
+		MenuBar menuBar = new MenuBar();
+		VBox topPosition = new VBox();
+		
+		
 		tc = new TekenCanvas(430, 300);
-
+		bp.setPrefSize(440, 125);
+		
+		//menuBestand setup
+		Menu menuBestand = new Menu("Bestand");
+		MenuItem nieuw = new MenuItem("Niew  CTRL + N");
+		menuBestand.getItems().add(nieuw);
+		
+		//menuOpties setup
+		Menu menuOpties= new Menu("Opties");
+		
+		//MenuBar setup
+		menuBar.getMenus().addAll(menuBestand,menuOpties);
+		menuBar.setPrefSize(440, 10);
+			
+		
 		root.setCenter(tc);
 		tc.setStyle("-fx-border-color: black;");
-		root.setTop(bp);
+		
+		topPosition.getChildren().addAll(menuBar,bp);
+		
+		root.setTop(topPosition);
 		scene.setRoot(root);
 		primaryStage.setTitle("Paint");
 		primaryStage.setScene(scene);
