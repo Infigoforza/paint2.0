63c0a43b669f4e71caf1a2bae36a70b9cb637d65
MartijnDevNull <m.balsmeer@gmail.com>
2015-03-17 14:50:22 +0100

    WERKEND DOOR DE MARTI MEESTER

--- src/paint/InstellingenDialoog.java
+++ src/paint/InstellingenDialoog.java
@@ -5,42 +5,33 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class InstellingenDialoog {
+public class InstellingenDialoog extends Stage{
 	private TextField inputHeight, inputWidth;
 	private Button annuleren, ok;
 	private int height, width;
 	private Label labelH, labelW;
 
-	public InstellingenDialoog(Stage venster, int height, int width) {
+	public InstellingenDialoog(Stage owner, int height, int width) {
+		initOwner(owner);
 		this.height = height;
 		this.width = width;
-		BorderPane root = new BorderPane();
+		FlowPane root = new FlowPane();
 		Scene scene = new Scene(root, width, height);
-
 		inputHeight = new TextField();
-		
 		inputWidth = new TextField();
 		annuleren = new Button("Anuleren");
 		ok = new Button("Ok");
 		labelH = new Label("Height");
 		labelW = new Label("Width");
-				
-		HBox center = new HBox();
-		
-		center.setPrefSize(200, 200);
-		center.getChildren().addAll(labelH,inputHeight,labelW,inputWidth,annuleren,ok);
-		
-		root.getChildren().addAll(center);
+		root.getChildren().addAll(labelH,inputHeight,labelW,inputWidth,annuleren,ok);
 		scene.setRoot(root);
-		venster.setTitle("Instellingen");
-		venster.setScene(scene);
-		venster.show();
+		this.setTitle("Instellingen");
+		this.setScene(scene);
+		this.showAndWait();
 
 	}
-
-	
-
 }
